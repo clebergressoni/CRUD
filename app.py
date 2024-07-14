32fd6cc6a1c5aa9a60a13adc8ecca1347e5864eb
cleber gressoni <cleberagressoni@gmail.com>
2024-07-14 20:17:38 -0300

    add post request

--- app.py
+++ app.py
@@ -3,15 +3,20 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def get_user():
+def get_user(id):
     user_data = {
-        'name':'cleber',
+        'name': 'teste',
         'age': 12,
-        'email': 'cleber@gmail.com'        
+        'email': 'cleber@gmail.com'   
     }
 
     return jsonify(user_data), 200
 
+@app.route('/create-user', methods = ['POST'])
+def create_user():
+    data = request.get_json()
+
+    return jsonify(data), 201
 
 
 if __name__ == "__main__":
